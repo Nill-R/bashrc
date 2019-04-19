6f14049049340e8736c6bc894cb70bcb8c7e19d7
Nill Rinov <nill@ingolonde.pw>
2019-04-19 13:58:16 +0200

    Small fix

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bashrc
+# My bashrc
+Usage: ```apt -y install bash-completion;git clone https://github.com/Nill-R/bashrc.git; ./bashrc/enable_bashrc.bash; source $HOME/.bashrc```
--- enable_bashrc.bash
+++ enable_bashrc.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
+if ! [ -d $HOME/.ssh ] ; then
+	mkdir $HOME/.ssh;chmod 600 $HOME/.ssh
+fi
 cd $HOME/bashrc/
 mv ~/.bashrc ~/.bashrc.old
 ln -s $PWD/bashrc ~/.bashrc 
 ln -s $PWD/bash_alias ~/.bash_alias
-source ~/.bashrc
